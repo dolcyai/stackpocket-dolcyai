15821ba199fc544dff0cf28be095c5c0283590af
dolcyai <dolcyai@gmail.com>
2024-06-21 16:31:52 +0000

    hjsahdbs

--- testformu.js
+++ testformu.js
@@ -1,20 +1,10 @@
-function TestFormu(array,contraints=[],toform=1,isSpan=false,spanType=3) {
-  if(isSpan){
-     const selected_num = array[0];
-     const total = selected_num.map((number=>span(number,spanType))).reduce((a,b)=>a+b) // 
-     return total; // total number of span bets
-  }else{
+class Person {
+  constructor(name, age) {
+      this.name = name;
+      this.age = age;
+  }
 
-    if(array.length === 1 || array.length > 2){
-      const multiple_numbers  = []
-      array.map((selection,index)=>{
-        let combinations_of_rows = getCombinations(selection,contraints[index])
-        multiple_numbers.push(combinations_of_rows.length)
-      })
-      return multiple_numbers.reduce((accumulator,currentValue)=>accumulator * currentValue) * toform
-    }else{
-
-    }
-    
-  }};
-export default TestFormu;
+  greet() {
+      return `Hello, ||||| my name is ${this.name}`;
+  }
+}
